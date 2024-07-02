de99168306aa5a21595a442e7b2d437696edd224
daartz <daartz@msn.com>
2024-07-03 01:51:26 +0200

    TWS

--- source/pythonclient/tests/reelOrder_V2.py
+++ source/pythonclient/tests/reelOrder_V2.py
@@ -36,12 +36,12 @@ def process_orders(port, index_list, sendMail=True):
     for country in index:
         print(country)
 
-        # if country not in ["USX", "EUROX"]:
-        #     if opening_hours(country) == False:
-        #         continue
-        #
-        #     if closing_hours(country) == False:
-        #         continue
+        if country not in ["USX", "EUROX"]:
+            if opening_hours(country) == False:
+                continue
+
+            if closing_hours(country) == False:
+                continue
 
         for order in orders:
 
